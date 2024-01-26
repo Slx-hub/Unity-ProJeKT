6c929d10ba9ee5715dc269fa6c4958b6a27d883a
Hyatukate <krahe.tobias@gmail.com>
2024-01-26 12:36:16 +0100

    added a visual aid for rigidbodys. Shows direction and magnitude of velocity and angualr velocity

---
+++ Assets/D20/Scripts/RigidbodyDebug.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class RigidbodyDebug : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody= GetComponent<Rigidbody>();
+    }
+
+    private void Reset()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        var vel = rigidbody.velocity;
+        var aVel = rigidbody.angularVelocity;
+
+        Gizmos.color = Color.green;
+        DrawArrow(transform.position, transform.position + vel);
+        Gizmos.color = Color.red;
+        DrawArrow(transform.position, transform.position + aVel);
+    }
+
+    private void DrawArrow(Vector3 lStart, Vector3 lEnd)
+    {
+        var lLen = (lEnd - lStart).magnitude;
+        var lDir = (lEnd - lStart).normalized;
+        var cDir = Camera.main.transform.forward;
+        var oDir = Camera.main.transform.right;
+
+        var leftArrowPoint = lStart + lDir * lLen * 0.875f + oDir * lLen * 0.125f;
+        var rightArrowPoint = lStart + lDir * lLen * 0.875f - oDir * lLen * 0.125f;
+
+        Gizmos.DrawLine(lStart, lEnd);
+        Gizmos.DrawLine(lEnd, leftArrowPoint);
+        Gizmos.DrawLine(lEnd, rightArrowPoint);
+    }
+}
