28fc7e3baf1f12a300fbec6069983c4c9a063656
Slx-hub <schneja@tuta.io>
2024-01-27 15:06:11 +0100

    Added camera clipping Fixed arena model

--- Assets/D20/Scripts/CameraControl.cs
+++ Assets/D20/Scripts/CameraControl.cs
@@ -12,28 +12,60 @@ public class CameraControl : MonoBehaviour
 
     private float ZOffset;
     private Vector2 CurrentRotation;
+    private Transform camera;
+    private float cameraDistance;
 
     void Awake()
     {
         ZOffset = transform.position.y - Target.position.y;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        camera = transform.GetChild(0);
+        cameraDistance = camera.localPosition.z;
     }
 
     void Update()
     {
+        UpdatePosition();
+        UpdateMouse();
+        CollideWithScene();
+        ShowCursorOnAlt();
+    }
+
+    private void UpdatePosition()
+    {
         var targetPos = Target.position + Vector3.up * ZOffset;
         transform.position = Vector3.Lerp(transform.position, targetPos, Lerp * Time.deltaTime);
+    }
+
+    private void UpdateMouse()
+    {
         CurrentRotation.x += Input.GetAxis("Mouse X") * MouseSensitivity;
         CurrentRotation.y -= Input.GetAxis("Mouse Y") * MouseSensitivity;
         CurrentRotation.x = Mathf.Repeat(CurrentRotation.x, 360);
         CurrentRotation.y = Mathf.Clamp(CurrentRotation.y, LowerAngularLimit, UpperAngularLimit);
         transform.rotation = Quaternion.Euler(CurrentRotation.y, CurrentRotation.x, 0);
+    }
 
+    private void CollideWithScene()
+    {
+        RaycastHit hit = new RaycastHit();
+        if (Physics.Raycast(transform.position, -transform.forward, out hit, Mathf.Abs(cameraDistance)))
+        {
+            camera.localPosition = new Vector3(0, 0, -hit.distance);
+        }
+        else
+        {
+            camera.localPosition = new Vector3(0, 0, cameraDistance);
+        }
+    }
+
+    private void ShowCursorOnAlt()
+    {
         if (!Application.isEditor)
             return;
 
-        if(Input.GetKeyDown(KeyCode.LeftAlt))
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
         {
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
