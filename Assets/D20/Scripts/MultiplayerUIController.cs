d7f9d63d13e47c401e56671ca59d4ad2fd7eacc5
Hyatukate <krahe.tobias@gmail.com>
2025-12-21 22:13:47 +0100

    Adding listener functionality to MultiplayerUIController to make it more generic

--- Assets/D20/Scripts/MultiplayerUIController.cs
+++ Assets/D20/Scripts/MultiplayerUIController.cs
@@ -1,7 +1,12 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using TMPro;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class MultiplayerUIController : MonoBehaviour
@@ -12,23 +17,22 @@ public class MultiplayerUIController : MonoBehaviour
     public TMP_InputField PortInput;
     public TMP_InputField IPInput;
 
-    void Awake()
-    {
-        HostButton.onClick.AddListener(() =>
-        {
-            NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData("127.0.0.1", (ushort)int.Parse(PortInput.text), "0.0.0.0");
-            NetworkManager.Singleton.StartHost();
-        });
+    public List<UnityAction> HostButtonCallbackList = new();
+    public List<UnityAction> JoinButtonCallbackList = new();
 
-        JoinButton.onClick.AddListener(() =>
-        {
-            NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(IPInput.text, (ushort)int.Parse(PortInput.text));
-            NetworkManager.Singleton.StartClient();
-        });
+    public void HostButton_Callback ()
+    {
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData("127.0.0.1", (ushort)int.Parse(PortInput.text), "0.0.0.0");
+        NetworkManager.Singleton.StartHost();
+
+        HostButtonCallbackList.ForEach(m => m());
     }
 
-    void Update()
+    public void JoinButton_Callback()
     {
-        
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(IPInput.text, (ushort)int.Parse(PortInput.text));
+        NetworkManager.Singleton.StartClient();
+
+        JoinButtonCallbackList.ForEach(m => m());
     }
 }
